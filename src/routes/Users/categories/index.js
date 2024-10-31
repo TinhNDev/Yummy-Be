524f6c0d19998c0b60c950cd2ee58111446fd2b8
Nguyễn Thanh Tịnh <142908230+MrToInh@users.noreply.github.com>
2024-11-01 00:17:30 +0700

    new update

--- src/routes/Users/categories/index.js
+++ src/routes/Users/categories/index.js
@@ -3,9 +3,9 @@ const express = require('express');
 const { asyncHandle } = require('../../../helper/asyncHandler');
 const CatergoriesController = require('../../../controllers/Users/categories.controller');
 const router = express.Router();
-router.get("/categories/all",asyncHandle(CatergoriesController.getAllCategories))
-router.get("/categories/:id",asyncHandle(CatergoriesController.getAllCategories))
+router.get("/categories", asyncHandle(CatergoriesController.getAllCategories));
+router.get("/categories/:id", asyncHandle(CatergoriesController.getCategoryById));
 
 router.get("/categories/product/all",asyncHandle(CatergoriesController.getListProductById));
-router.get("/categories/product/:id",asyncHandle(CatergoriesController.getProductByCategoryId));
+router.get("/categories/:id/products", asyncHandle(CatergoriesController.getProductByCategoryId));
 module.exports = router;
