b61ed680f5bf207869dd877406f7824f227bfaff
Nguyễn Thanh Tịnh <nttinh102003@gmail.com>
2024-11-04 19:50:56 +0700

    123

--- src/configs/config.mysql.js
+++ src/configs/config.mysql.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 require("dotenv").config();
 module.exports = {
   HOST: process.env.HOST,
@@ -13,19 +12,3 @@ module.exports = {
     idle: process.env.IDLE,
   },
 };
-=======
-require("dotenv").config();
-module.exports = {
-  HOST: process.env.HOST,
-  USER: process.env.USER,
-  PASSWORD: process.env.PASSWORD,
-  DB: process.env.DB,
-  dialect: process.env.DIALECT,
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: process.env.ACQUIRE,
-    idle: process.env.IDLE,
-  },
-};
->>>>>>> ba1ec96e9f13d8946d170ae05d9691d1754d1aa7
