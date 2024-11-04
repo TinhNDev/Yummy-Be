8e9dd3d05608d989ce9a55d6425af2ab5db8126c
Nguyễn Thanh Tịnh <nttinh102003@gmail.com>
2024-11-04 19:54:44 +0700

    123

--- src/models/Users/Drivers/driver.model.js
+++ src/models/Users/Drivers/driver.model.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 module.exports = (sequelize, Sequelize) => {
   const Driver = sequelize.define("Driver", {
     vehicle_type: {
@@ -21,27 +20,3 @@ module.exports = (sequelize, Sequelize) => {
   });
   return Driver;
 };
-=======
-module.exports = (sequelize, Sequelize) => {
-  const Driver = sequelize.define("Driver", {
-    vehicle_type: {
-      type: Sequelize.ENUM,
-      values: ["motorbike", "car"],
-    },
-    license_plate: {
-      type: Sequelize.STRING,
-    },
-    current_location: {
-      type: Sequelize.STRING,
-    },
-    status: {
-      type: Sequelize.ENUM,
-      values: ["active", "warming", "inactive"],
-    },
-    phone_number: {
-      type: Sequelize.INTEGER,
-    },
-  });
-  return Driver;
-};
->>>>>>> ba1ec96e9f13d8946d170ae05d9691d1754d1aa7
