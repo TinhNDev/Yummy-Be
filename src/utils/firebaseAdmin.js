41e1955148da033bc177d539b3bb723e26d0bd00
Nguyễn Thanh Tịnh <nttinh102003@gmail.com>
2024-11-04 19:59:54 +0700

    123

--- src/utils/firebaseAdmin.js
+++ src/utils/firebaseAdmin.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const admin = require('firebase-admin');
 const serviceAccount = require('../../fbconfig.json');
 
@@ -7,13 +6,3 @@ admin.initializeApp({
 });
 
 module.exports = admin;
-=======
-const admin = require('firebase-admin');
-const serviceAccount = require('../../fbconfig.json');
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
-module.exports = admin;
->>>>>>> ba1ec96e9f13d8946d170ae05d9691d1754d1aa7
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 const _ = require("lodash");
 const getInforData = ({ fileds = [], object = {} }) => {
   return _.pick(object, fileds);
 };
 module.exports=getInforData;
-=======
-const _ = require("lodash");
-const getInforData = ({ fileds = [], object = {} }) => {
-  return _.pick(object, fileds);
-};
-module.exports=getInforData;
->>>>>>> ba1ec96e9f13d8946d170ae05d9691d1754d1aa7
