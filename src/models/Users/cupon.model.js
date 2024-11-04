8c9358cc469544d4db4c9ab99a2418d9f0a983ff
Nguyễn Thanh Tịnh <nttinh102003@gmail.com>
2024-11-04 19:55:50 +0700

    123

--- src/models/Users/cupon.model.js
+++ src/models/Users/cupon.model.js
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 module.exports = (sequelize, Sequelize) => {
   const Cupon = sequelize.define("Cupon", {
     cupon_code: Sequelize.STRING,
   });
   return Cupon;
 };
-=======
-module.exports = (sequelize, Sequelize) => {
-  const Cupon = sequelize.define("Cupon", {
-    cupon_code: Sequelize.STRING,
-  });
-  return Cupon;
-};
->>>>>>> ba1ec96e9f13d8946d170ae05d9691d1754d1aa7
