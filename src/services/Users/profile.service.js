31781b2a96095cf54e0d3e4b95e7d7a89d0cf424
Nguyễn Thanh Tịnh <nttinh102003@gmail.com>
2024-11-17 15:22:31 +0700

    new update

--- src/services/Users/profile.service.js
+++ src/services/Users/profile.service.js
@@ -4,18 +4,19 @@ const { CreateAddress } = require("./address.service");
 const Profile = db.Profile;
 const Address = db.Address;
 class ProfileService {
-  static UpdateProfile = async ({ user_id, body, address }) => {
+  static UpdateProfile = async ({ user_id, body,email }) => {
     let profile = await Profile.findOne({ where: { user_id: user_id } });
     if (profile) {
         await Profile.update({
         name: body.name,
         image: body.image,
         date: body.date,
+        cic:body.cic,
         phone_number: body.phone_number,
-        mail: body.mail,
+        mail: email,
         user_id: user_id,
       },
-      {where:{id:user_id}}
+      {where:{user_id:user_id}}
     );
     } else {
          await Profile.create({
@@ -23,22 +24,18 @@ class ProfileService {
         image: body.image,
         date: body.date,
         phone_number: body.phone_number,
-        mail: body.mail,
+        mail: email,
+        cic:body.cic,
         user_id: user_id,
       });
     }
-    if (address) {
-      await CreateAddress(address,user_id);
-    }
     return {
-      profile: await Profile.findOne({ where: { user_id: user_id } }),
-      address: await Address.findAll({where:{profileId:user_id}})
+      Profile: await Profile.findOne({ where: { user_id: user_id } }),
     };
   };
   static getProfie = async (user_id) =>{
     return {
       profile:await Profile.findOne({where:{id: user_id}}),
-      address: await Address.findAll({where:{profileId:user_id}}),
     }
   }
 }
