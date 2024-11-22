29fe102743a64d7e31d13247bb0b874ae8e105e4
Nguyễn Thanh Tịnh <nttinh102003@gmail.com>
2024-11-22 15:43:03 +0700

    123

--- src/services/Users/Customers/index.service.js
+++ src/services/Users/Customers/index.service.js
@@ -2,16 +2,9 @@ const { Customer, Order, Profile, Order } = require("../../../models/index.model
 
 class CustomerService {
   static getAllOrderForCustomer = async ({ user_id }) => {
-    const Profile = findOne({where:{user_id:user_id}})
-    const Order = await Customer.findAll({
-      where: { profile_id: Profile.id },
-      includes: [
-        {
-          model: Order,
-        },
-      ],
-    });
-    return await Order.Customer.Order;
+    const Profile =await findOne({where:{user_id:user_id}})
+    const Customer =await findOne({where:{profile_id: Profile.id}}) 
+    return await Order.findOne({where:{customer_id:Customer.id}})
   };
   static getOrderForCustomer = async ({ user_id, order_id }) => {
     const Customer = await Customer.findOne({ where: { user_id: user_id } });
