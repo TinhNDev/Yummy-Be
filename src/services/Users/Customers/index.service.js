865d8997ac26b5059f682bffff85c92bfd9acd3f
Nguyễn Thanh Tịnh <nttinh102003@gmail.com>
2024-11-23 18:05:17 +0700

    123

--- src/services/Users/Customers/index.service.js
+++ src/services/Users/Customers/index.service.js
@@ -1,24 +1,38 @@
-const { Customer, Profile, Order, Restaurant } = require("../../../models/index.model");
+const {
+  Customer,
+  Profile,
+  Order,
+  Restaurant,
+  Driver,
+} = require("../../../models/index.model");
 
 class CustomerService {
-  static getAllOrderForCustomer = async ({ user_id }) => { 
-    const Profile = await Profile.findOne({ where: { user_id: user_id } });
-    if (!Profile) throw new Error("Profile not found");
-    
-    const Customer = await Customer.findOne({ where: { profile_id: Profile.id } });
-    if (!Customer) throw new Error("Customer not found");
-    
-    return await Order.findAll({
-        where: { customer_id: Customer.id },
-        include: [
-            {
-                model: Restaurant,
-                attributes: [], // Lấy thuộc tính cần thiết nếu có
-            },
-        ],
-        order: [['createdAt', 'DESC']], // Sắp xếp theo createdAt, giảm dần để lấy order mới nhất
+  static getAllOrderForCustomer = async ({ user_id }) => {
+    const profile = await Profile.findOne({ where: { user_id: user_id } });
+    if (!profile) throw new Error("Profile not found");
+
+    const customer = await Customer.findOne({
+      where: { profile_id: profile.id },
     });
-};
+    if (!customer) throw new Error("Customer not found");
+
+    return await Order.findAll({
+      where: { customer_id: customer.id },
+      include: [
+        {
+          model: Restaurant,
+          attribute: [], // Lấy thuộc tính cần thiết nếu có
+        },
+      ],
+      include: [
+        {
+          model: Driver,
+          attribute: [],
+        },
+      ],
+      order: [["createdAt", "DESC"]], // Sắp xếp theo createdAt, giảm dần để lấy order mới nhất
+    });
+  };
 
   static getOrderForCustomer = async ({ user_id, order_id }) => {
     const Customer = await Customer.findOne({ where: { user_id: user_id } });
