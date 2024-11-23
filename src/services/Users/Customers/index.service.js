d73118a98016f9538966f1ecbfb99a9749076d5d
Nguyễn Thanh Tịnh <nttinh102003@gmail.com>
2024-11-23 18:14:58 +0700

    123

--- src/services/Users/Customers/index.service.js
+++ src/services/Users/Customers/index.service.js
@@ -12,27 +12,26 @@ class CustomerService {
     if (!profile) throw new Error("Profile not found");
 
     const customer = await Customer.findOne({
-      where: { profile_id: profile.id },
+        where: { profile_id: profile.id },
     });
     if (!customer) throw new Error("Customer not found");
 
     return await Order.findAll({
-      where: { customer_id: customer.id },
-      include: [
-        {
-          model: Restaurant,
-          attribute: [], // Lấy thuộc tính cần thiết nếu có
-        },
-      ],
-      include: [
-        {
-          model: Driver,
-          attribute: [],
-        },
-      ],
-      order: [["createdAt", "DESC"]], // Sắp xếp theo createdAt, giảm dần để lấy order mới nhất
+        where: { customer_id: customer.id },
+        include: [
+            {
+                model: Restaurant,
+                attributes: [],
+            },
+            {
+                model: Driver,
+                attributes: [],
+            },
+        ],
+        order: [["createdAt", "DESC"]],
     });
-  };
+};
+
 
   static getOrderForCustomer = async ({ user_id, order_id }) => {
     const Customer = await Customer.findOne({ where: { user_id: user_id } });
