2e6d48002e96603e4412bb370b669f9bbd6a1d37
Nguyễn Thanh Tịnh <nttinh102003@gmail.com>
2024-11-25 22:02:06 +0700

    update

--- src/services/Users/reviews.service.js
+++ src/services/Users/reviews.service.js
@@ -1,15 +1,17 @@
 const db = require("../../models/index.model");
 class ReviewService {
   static createReview = async ({ user_id, order_id, reviews }) => {
-    const order = await findOne({ where: { id: order_id } });
+    const order = await db.Order.findOne({ where: { id: order_id } });
     const Customer =await db.User.findOne({
       where: { id: user_id },
       includes: [
         {
           model: db.Profile,
+          as: "Profile",
           includes: [
             {
               model: db.Customer,
+              as: "Customer"
             },
           ],
         },
@@ -20,7 +22,7 @@ class ReviewService {
       dri_rating: reviews.dri_rating,
       res_comment: reviews.res_comment,
       dri_comment: reviews.dri_comment,
-      customer_id: Customer.User.Profile.Customer.id,
+      customer_id: Customer.Profile.Customer.id,
       driver_id: order.driver_id,
       restaurant_id: order.restaurant_id,
     });
