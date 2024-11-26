8304219efe319f30eb94f1a4ee611980c3f485c9
Nguyễn Thanh Tịnh <nttinh102003@gmail.com>
2024-11-26 19:24:25 +0700

    123

--- src/services/Users/Restaurants/index.service.js
+++ src/services/Users/Restaurants/index.service.js
@@ -15,7 +15,7 @@ const getAllDriverIdsFromRedis = require("../../../helper/redisFunction");
 const { io } = require("socket.io-client");
 const socket = io(process.env.SOCKET_SERVER_URL);
 const admin = require("firebase-admin");
-const redisClient = redis.createClient();
+const RedisHelper = require('../../../cache/redis')
 class OrderRestaurantService {
 
   static getOrder = async ({restaurant_id}) =>{
@@ -43,162 +43,157 @@ class OrderRestaurantService {
       orderId: order_id,
       status: "PREPARING_ORDER",
     });
+    const redisHelper = new RedisHelper();
     try {
-      if (!redisClient.isOpen) {
-        await redisClient.connect();
-      }
+      await redisHelper.connect(); // Connect to Redis
+
       const order = await Order.findByPk(parseInt(order_id));
       if (!order) throw new Error("Order not found");
-      const restaurant = await Restaurant.findOne({where:{id:order.restaurant_id}})
+
+      const restaurant = await Restaurant.findOne({ where: { id: order.restaurant_id } });
       const driverIds = await getAllDriverIdsFromRedis();
       let nearestDriver = null;
       let shortestDistance = Infinity;
-      let blacklist;
-      if(order.order_status=='ORDER_CANCELED'){
-        blacklist = await BlackList.findAll({where:{order_id:order_id}})
-      }
+      const blacklist = order.order_status === 'ORDER_CANCELED' 
+          ? await BlackList.findAll({ where: { order_id: order_id } }) 
+          : [];
+
       for (const driverId of driverIds) {
-        if(blacklist){
-          const isBlacklist = blacklist.some(
-            (entry)=>entry.driver_id == driverId && entry.status == true
-          )
-          if (isBlacklist) {
-            console.log(`Driver ${driverId} bị blacklist, bỏ qua.`);
-            continue;
+          if (blacklist.some(entry => entry.driver_id == driverId && entry.status)) {
+              console.log(`Driver ${driverId} is in the blacklist, skipping.`);
+              continue;
           }
-        }
-        if((await Driver.findOne({where:{id:driverId}})).status =='BUSY'){
-          continue;
-        }
-        const driverLocation = await redisClient.hGetAll(
-          `driver:${driverId}:location`
-        );
 
-        if (
-          driverLocation &&
-          driverLocation.latitude &&
-          driverLocation.longitude
-        ) {
-          const driverCoords = {
-            latitude: parseFloat(driverLocation.latitude),
-            longitude: parseFloat(driverLocation.longitude),
-          };
-          const restaurantCoords = {
-            latitude: parseFloat(restaurant.dataValues.address_x),
-            longitude: parseFloat(restaurant.dataValues.address_y),
-          };
-
-          const distance = geolib.getDistance(driverCoords, restaurantCoords);
-
-          if (distance < shortestDistance) {
-            shortestDistance = distance;
-            nearestDriver = driverId;
+          const driverStatus = await Driver.findOne({ where: { id: driverId } }).then(driver => driver?.status);
+          if (driverStatus === 'BUSY') {
+              continue;
           }
-        }
-      }
-      let fcmToken;
-      if (nearestDriver) {
-        const transaction = await sequelize.transaction();
 
-        try {
-          await Order.update(
-            { driver_id: nearestDriver, order_status: "PREPARING_ORDER" },
-            { where: { id: order.dataValues.id }, transaction }
-          );
-          await Driver.update({status:'BUSY'},{where:{id:nearestDriver}});
-          const updatedOrder = await Order.findOne({
-            where: { id: order.dataValues.id },
-            include: [
-              {
-                model: Restaurant,
-                attributes: ["id", "name", "address"],
-              },
-              {
-                model: Driver,
-                attributes: ["license_plate"],
-                include: [
-                  {
-                    model: Profile,
-                    as: "Profile",
-                    attributes: ["id", "name", "image", "phone_number", "cic"],
-                    include: [
-                      {
-                        model: User,
-                        as: "User",
-                        include: [
-                          {
-                            model: KeyToken,
-                            as: "Key Tokens",
-                            attributes: ["fcmToken"],
-                          },
-                        ],
-                      },
-                    ],
-                  },
-                ],
-              },
-            ],
-            transaction,
-          });
-          await transaction.commit();
-          fcmToken =
-            updatedOrder?.Driver?.Profile?.User?.["Key Tokens"]?.[0]?.fcmToken;
-          try {
-            if (fcmToken) {
-              const payload = {
-                notification: {
-                  title: `new order ${order_id}`,
-                  body: `Bạn có đơn hàng mới!`,
-                },
-                token: fcmToken,
+          const driverLocation = await redisHelper.get(`driver:${driverId}:location`);
+
+          if (driverLocation && driverLocation.latitude && driverLocation.longitude) {
+              const driverCoords = {
+                  latitude: parseFloat(driverLocation.latitude),
+                  longitude: parseFloat(driverLocation.longitude),
+              };
+              const restaurantCoords = {
+                  latitude: parseFloat(restaurant.dataValues.address_x),
+                  longitude: parseFloat(restaurant.dataValues.address_y),
               };
 
-              const response = await admin.messaging().send(payload);
-              console.log("Successfully sent message:", response);
-            } else {
-              console.log("FCM token not found");
-            }
-          } catch (error) {
-            console.error("Error sending notification:", error);
+              const distance = geolib.getDistance(driverCoords, restaurantCoords);
+              if (distance < shortestDistance) {
+                  shortestDistance = distance;
+                  nearestDriver = driverId;
+              }
           }
-          socket.emit("newOrderForDriver", {
-            data: updatedOrder?.dataValues,
-          });
-
-          return {
-            order: updatedOrder,
-          };
-        } catch (error) {
-          if (!transaction.finished) {
-            await transaction.rollback();
-          }
-          throw error;
-        }
-      } else {
-        try {
-          if (fcmToken) {
-            const payload = {
-              notification: {
-                title: `new order ${order_id}`,
-                body: `No available driver found`,
-              },
-              token: fcmToken,
-            };
-
-            const response = await admin.messaging().send(payload);
-            console.log("Successfully sent message:", response);
-          } else {
-            console.log("FCM token not found");
-          }
-        } catch (error) {
-          console.error("Error sending notification:", error);
-        }
       }
-    } catch (error) {
+
+      let fcmToken;
+      if (nearestDriver) {
+          const transaction = await sequelize.transaction();
+          try {
+              await Order.update(
+                  { driver_id: nearestDriver, order_status: "PREPARING_ORDER" },
+                  { where: { id: order.dataValues.id }, transaction }
+              );
+              await Driver.update({ status: 'BUSY' }, { where: { id: nearestDriver } });
+
+              const updatedOrder = await Order.findOne({
+                  where: { id: order.dataValues.id },
+                  include: [
+                      {
+                          model: Restaurant,
+                          attributes: ["id", "name", "address"],
+                      },
+                      {
+                          model: Driver,
+                          attributes: ["license_plate"],
+                          include: [
+                              {
+                                  model: Profile,
+                                  as: "Profile",
+                                  attributes: ["id", "name", "image", "phone_number", "cic"],
+                                  include: [
+                                      {
+                                          model: User,
+                                          as: "User",
+                                          include: [
+                                              {
+                                                  model: KeyToken,
+                                                  as: "Key Tokens",
+                                                  attributes: ["fcmToken"],
+                                              },
+                                          ],
+                                      },
+                                  ],
+                              },
+                          ],
+                      },
+                  ],
+                  transaction,
+              });
+              await transaction.commit();
+
+              fcmToken = updatedOrder?.Driver?.Profile?.User?.["Key Tokens"]?.[0]?.fcmToken;
+
+              try {
+                  if (fcmToken) {
+                      const payload = {
+                          notification: {
+                              title: `New order ${order_id}`,
+                              body: `You have a new order!`,
+                          },
+                          token: fcmToken,
+                      };
+
+                      const response = await admin.messaging().send(payload);
+                      console.log("Successfully sent message:", response);
+                  } else {
+                      console.log("FCM token not found");
+                  }
+              } catch (error) {
+                  console.error("Error sending notification:", error);
+              }
+
+              socket.emit("newOrderForDriver", { data: updatedOrder?.dataValues });
+
+              return {
+                  order: updatedOrder,
+              };
+          } catch (error) {
+              if (!transaction.finished) {
+                  await transaction.rollback();
+              }
+              throw error;
+          }
+      } else {
+          try {
+              if (fcmToken) {
+                  const payload = {
+                      notification: {
+                          title: `New order ${order_id}`,
+                          body: `No available driver found`,
+                      },
+                      token: fcmToken,
+                  };
+
+                  const response = await admin.messaging().send(payload);
+                  console.log("Successfully sent message:", response);
+              } else {
+                  console.log("FCM token not found");
+              }
+          } catch (error) {
+              console.error("Error sending notification:", error);
+          }
+      }
+  } catch (error) {
       console.error("Error in findDriver:", error.message);
       throw new Error("Could not find driver");
-    }
-  };
+  } finally {
+      await redisHelper.disconnect(); // Disconnect Redis
+  }
+};
 
   static rejectOrderByRestaurant = async ({
     restaurant_id,
