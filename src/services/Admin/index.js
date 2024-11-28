3ab0d5a8250dc540c11a7d920183ab2c83049b16
Nguyễn Thanh Tịnh <nttinh102003@gmail.com>
2024-11-28 15:50:35 +0700

    update res adn pro

--- src/services/Admin/index.js
+++ src/services/Admin/index.js
@@ -1,22 +1,23 @@
-const { Restaurant, Profile, Customer } = require("../../models/index.model");
+const { Restaurant, Profile, Driver, Order } = require("../../models/index.model");
 
 class AdminService {
   static getAllRestaurant = async () => {
     return Restaurant.findAll();
   };
   static getAllOrder = async () => {
-    return Restaurant.findAll();
+    return Order.findAll();
   };
   static getAllDriver = async () => {
-    const Driver = Driver.findAll({
+    const driver = Driver.findAll({
       include: [
         {
           model: Profile,
+          as:"Profile",
           attribute: [],
         },
       ],
     });
-    return Driver;
+    return driver;
   };
   static getAllCustomer = async () => {
     const Customer = Customer.findAll({
@@ -29,6 +30,21 @@ class AdminService {
     });
     return Customer;
   };
+
+  static changeStatusRestaurant = async(restaurant_id)=>{
+    const restaurant =await Restaurant.findOne({where:{id:restaurant_id}});
+    if(restaurant.status==='pending'){
+      restaurant.status ='active'
+    }else {
+      restaurant.status = 'pending';
+    }
+    return  restaurant.save();
+  }
+  static changeStatusDriver = async({driver_id,status})=>{
+    const driver = await Driver.findByPk(driver_id);
+    driver.status = status;
+    return await driver.save();
+  }
 }
 
 module.exports = AdminService;
