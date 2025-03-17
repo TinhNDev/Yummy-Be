0ca38c3bfe97a51fda21350db40dd7cdcd83c9d6
Thanh Tinh <nttinh102003@gmail.com>
2025-03-17 09:26:12 +0700

    fix dang ki

--- src/services/Users/access.service.js
+++ src/services/Users/access.service.js
@@ -20,16 +20,7 @@ const {
 } = require("../../utils/emailTemplate");
 class AccessService {
   static singUp = async ({ password, email, fcmToken, role }) => {
-    const holderUser = await user.findOne({
-      where: {
-        email,
-      },
-      include: {
-        model: db.Roles,
-        as: 'roles',
-        through: { attributes: [] },
-      },
-    });
+    const holderUser = await findRoleByEmail({email});
     if (holderUser) {
       const hasRole =
         holderUser.roles[0]?.dataValues.name === role ? true : false;
