a86fbfe2700d067686d957a0b2109382bbca5306
Nguyễn Thanh Tịnh <nttinh102003@gmail.com>
2025-03-20 17:06:16 +0000

    fixbug

--- src/services/Users/reviews.service.js
+++ src/services/Users/reviews.service.js
@@ -2,19 +2,14 @@ const db = require("../../models/index.model");
 class ReviewService {
   static createReview = async ({ user_id, order_id, reviews }) => {
     const order = await db.Order.findOne({ where: { id: order_id } });
-    const customer = await db.Customer.findOne({
-      include: {
-        model: db.Profile,
-        where: { user_id: user_id },
-      },
-    });
-    
+    const profile = await db.Profile.findOne({where:{user_id:user_id}})
+    const Customer = await db.Customer.findOne({where:{profile_id: profile.id}})
     return await db.Review.create({
       res_rating: reviews.res_rating,
       dri_rating: reviews.dri_rating,
       res_comment: reviews.res_comment,
       dri_comment: reviews.dri_comment,
-      customer_id: customer.id,
+      customer_id: Customer.id,
       driver_id: order.driver_id,
       restaurant_id: order.restaurant_id,
     });
