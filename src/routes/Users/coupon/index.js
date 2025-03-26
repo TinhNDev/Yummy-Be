05c2ef3fdb1cece01b6d4f11bce3e7f9de4427a5
Nguyễn Thanh Tịnh <nttinh102003@gmail.com>
2025-03-26 09:24:02 +0000

    update coupon

--- src/routes/Users/coupon/index.js
+++ src/routes/Users/coupon/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const { asyncHandle } = require("../../../helper/asyncHandler");
 const couponController = require("../../../controllers/Users/coupon.controller");
+const { authorization, checkRole } = require("../../../auth/authUtils");
 
 const router = express.Router();
 
-router.get("/coupon", asyncHandle(couponController.getCoupon));
+router.post("/coupon",authorization,checkRole([admin]),asyncHandle(couponController.createCoupon));
+router.post("/coupon/:coupon_name/customer",authorization, asyncHandle(couponController.searchCoupon));
+router.get("/coupon",authorization,asyncHandle(couponController.getCoupon));
 
 module.exports = router;
