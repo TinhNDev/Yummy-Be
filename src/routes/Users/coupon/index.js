fbccb62dcc457940ba0a947ee8868eb6cc82fb94
Nguyễn Thanh Tịnh <nttinh102003@gmail.com>
2025-03-26 11:43:37 +0000

    update

--- src/routes/Users/coupon/index.js
+++ src/routes/Users/coupon/index.js
@@ -7,6 +7,6 @@ const router = express.Router();
 
 router.post("/coupon",authorization,checkRole(['admin']),asyncHandle(couponController.createCoupon));
 router.post("/coupon/:coupon_name/customer",authorization, asyncHandle(couponController.searchCoupon));
-router.get("/coupon",authorization,asyncHandle(couponController.getCoupon));
+router.get("/coupon/:totalCost",authorization,asyncHandle(couponController.getCoupon));
 
 module.exports = router;
